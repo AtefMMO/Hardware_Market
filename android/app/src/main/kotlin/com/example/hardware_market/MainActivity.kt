9a2cc1b6be96d16329fff5486795d969a2e008c8
AtefMMO <mohamedatefimb@gmail.com>
2024-07-05 06:49:30 +0300

    App starts

---
+++ android/app/src/main/kotlin/com/example/hardware_market/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hardware_market
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:hardware_market/main_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MainLayout(),
+    );
+  }
+}
