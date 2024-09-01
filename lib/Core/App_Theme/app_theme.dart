a7a34bf631b312c950cd456a71a8eae73c6d8dce
AtefMMO <mohamedatefimb@gmail.com>
2024-09-01 21:17:20 +0300

    signup ui done login  ui done

--- lib/Core/App_Theme/app_theme.dart
+++ lib/Core/App_Theme/app_theme.dart
@@ -6,26 +6,29 @@ class AppTheme {
   static Color secondaryColor = const Color(0xffffffff);
   static Color selectedBottomNavBar = const Color(0xff67C4A7);
   static Color unselectedBottomNavBar = const Color(0xff939393);
+  static Color blueColor = const Color(0xff3780ED);
+  static Color redColor = const Color(0xffFF0000);
+
   static final ThemeData appTheme = ThemeData(
       textTheme: const TextTheme(
           titleLarge: TextStyle(
               fontSize: 26,
-              color:  Color(0xff878787),
+              color: Color(0xff878787),
               fontWeight: FontWeight.w700),
           bodyLarge: TextStyle(
             fontSize: 14,
-            color:  Color(0xff393F42),
+            color: Color(0xff393F42),
           ),
           bodyMedium: TextStyle(
             fontSize: 12,
-            color:  Color(0xff393F42),
+            color: Color(0xff393F42),
           ),
           bodySmall: TextStyle(
             fontSize: 12,
-            color:  Color(0xff939393),
+            color: Color(0xff939393),
           ),
           titleMedium: TextStyle(
               fontSize: 14,
-              color:Color(0xff393F42),
+              color: Color(0xff393F42),
               fontWeight: FontWeight.bold)));
 }
