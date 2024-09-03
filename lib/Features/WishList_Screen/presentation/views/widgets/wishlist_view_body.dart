098c7a413159f04a2879d287e46377908e11275d
AtefMMO <mohamedatefimb@gmail.com>
2024-09-03 22:48:53 +0300

    wishlist ui done

--- lib/Features/WishList_Screen/presentation/views/widgets/wishlist_view_body.dart
+++ lib/Features/WishList_Screen/presentation/views/widgets/wishlist_view_body.dart
@@ -1,16 +1,33 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'wishlist_products_column.dart';
 
 class WishListViewBody extends StatelessWidget {
   const WishListViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('WishListViewBody'),
-        ],
-      ),
+    return NestedScrollView(
+      headerSliverBuilder: (context, innerBoxIsScrolled) => [
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 40.h,
+                ),
+                Text(
+                  'WishList',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+      body: WishListProductsColumn(),
     );
   }
 }
